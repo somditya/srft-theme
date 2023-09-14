76b64a7f5a5ffa21c0584badd3ba1c6fdd984f4b
Somaditya Majumder <somditya@gmail.com>
2023-09-14 18:00:18 +0530

    Update

--- single-news.php
+++ single-news.php
@@ -17,35 +17,33 @@ $post_content = apply_filters('the_content', $post->post_content);
 
 ?>
 
-<div style="margin: 15rem;  max-width: 1250px;">
-<div style="display: flex;">
-<div style="flex: 30% 1; max-width: 30%;">
-
-
-<img src="<?php the_post_thumbnail_url('thumbnail'); ?>" width="220" height="220" alt="" />
-
-</div>
-<div style="background-color: rgb(243, 243, 243);
-    flex: 1 1 70%;
-    max-width: 70%;
-    padding: 16px 24px;
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 28px;">
-   <h1 style="
-    font-weight: 400;
-    margin: 0 0 4rem;
-    font-size: 4rem;
-    line-height: 5rem;
-    color: #8b5b2b;
-    text-transform: uppercase;
-    margin-bottom: 2.5rem;
-    padding-bottom: 2rem;
-    border-bottom: 2px solid #d6d6ce;"><?php echo get_the_title($post_id);?></h1>   
-<?php echo $post_content; ?>
-</div>
-
+<section style="padding: 0 1.25rem;">
+    <article style="display: grid; grid-gap: 1.875rem;
+    grid-template-columns: 1fr 6.7% 56.67% 6.7% 1fr;">
+        <header role="article" style="text-align: center;max-width: 56.25rem;
+    padding-top: 1.25rem; grid-column: 3;">
+            <a href="#" alt="News" class="c-headline__topic t-heading--topic">
+  News</a> 
+  <h1 style="font-size: 4.375rem;
+    letter-spacing: -.0875rem;line-height: 1.1;
+    margin: 0;
+    font-style: normal;
+    font-weight: 500;">
+  <?php echo get_the_title($post_id);?>
+</h1> 
+<p class="c-headline__date">
+    <time datetime="2023-08-08T12:00:00Z"><?php ?></time></p>
+</header> 
+<div stle="grid-column: 2/5;">
+    <div class="c-photo">
+    <img src="<?php the_post_thumbnail_url('thumbnail'); ?>" width="220" height="220" alt="" />
 </div>
 </div>
+<div style="">
+</div>
+<div style="">
+</div>
+</article>       
+</section> 
   
 <?php get_footer(); ?>
