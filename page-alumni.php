8d2dd5ca52a050adfe6de637b40974ab6d56fd0b
Somaditya Majumder <somditya@gmail.com>
2023-10-24 21:10:21 +0530

    Updates

--- page-alumni.php
+++ page-alumni.php
@@ -1,20 +1,21 @@
 <?php
 
 /*
-Template Name: News
+Template Name: NewsList
 
  */
 
-
+ 
 ?>
 
 <?php
-get_header(); 
+get_header();
 
 function get_category_ID( $cat_name ) { // phpcs:ignore WordPress.NamingConventions.ValidFunctionName.FunctionNameInvalid
 	$cat = get_term_by( 'name', $cat_name, 'category' );
-
+    echo '<script type="text/javascript">alert("Category found: ' . $cat_name . '");</script>';
 	if ( $cat ) {
+        echo '<script type="text/javascript">alert("Category found: ' . $cat_name . '");</script>';
 		return $cat->term_id;
 	}
 
@@ -23,6 +24,7 @@ function get_category_ID( $cat_name ) { // phpcs:ignore WordPress.NamingConventi
 
 $category_name = 'news'; // Ensure this matches the exact category name.
 $category_id = get_category_ID($category_name);
+echo "Hello";
 ?>
 
 <body ng-controller="NewsController">
@@ -37,7 +39,6 @@ $category_id = get_category_ID($category_name);
             <div class="container" style="width: 1170px;">
                 <h2 class="page-header-text" style="padding-left: 0; text-align: center;"><?php echo __('News', 'srft-theme' ); ?></h2>
                 <div ng-app="myApp" ng-controller="NewsController" style="margin-top: 4.5rem;">
-
                     <!-- News grid without pagination -->
                     <div class="news-grid">
                         <a class="news-card" ng-repeat="news in newsList">
@@ -59,12 +60,12 @@ $category_id = get_category_ID($category_name);
         </section>
 
         <script>
-            console.log('Inside');
             var categoryID = <?php echo json_encode($category_id); ?>;
             var siteURL = '<?php echo esc_url(site_url('/')); ?>';
 
             angular.module('myApp', [])
                 .controller('NewsController', function($scope, $http) {
+                   
                     // Initialize the news data
                     $http.get(siteURL + 'wp-json/wp/v2/posts?categories=' + categoryID + '&per_page=100') // Adjust 'per_page' as needed
                         .then(function(response) {
@@ -100,7 +101,3 @@ $category_id = get_category_ID($category_name);
         </script>
           
     </main>
-
-<?php
-get_footer(); 
-?>
