b871e2e142b6c46fb405879f3a5758d060711006
Somaditya Majumder <somditya@gmail.com>
2024-09-19 15:40:18 +0530

    Update

--- page-home.php
+++ page-home.php
@@ -787,39 +787,48 @@ endif;
 <div class="cell">
       <span class="update-title" ><?php echo __('Vacancy', 'srft-theme' ); ?></span>
       <?php
-       if ($current_language === 'en_US') {
-        $catslug='vacancy'; 
-       }
-        else
-        {
-          $catslug='vacancy-hi';
+if ($current_language === 'en_US') {
+    $catslug = 'tender';
+} else {
+    $catslug = 'tender-hi';
+}
+
+$category_posts = new WP_Query(array(
+    'post_type' => 'tender',
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'category',
+            'field'    => 'slug',
+            'terms'    => $catslug,
+        ),
+    ),
+    'posts_per_page' => 5,
+));
+
+if ($category_posts->have_posts()) :
+    while ($category_posts->have_posts()) : $category_posts->the_post();
+        // Retrieve the ACF field 'NIT-DOC' which returns an array
+        $nit_doc = get_field('Tender-Doc');
+        
+        if ($nit_doc && isset($nit_doc['url'])) {
+            // Use the 'url' key from the array
+            $link = esc_url($nit_doc['url']);
+        } else {
+            // Fallback to post permalink if NIT-DOC URL is not available
+            $link = get_permalink();
         }
-    $category_posts = new WP_Query(array(
-      'post_type' => 'vacancy',
-      'tax_query' => array(
-          array(
-              'taxonomy' => 'category',
-              'field'    => 'slug',
-              'terms'    => $catslug,
-          ),
-      ),
-      'posts_per_page' => 5,
-  ));
+?>
+        <h3>
+            <a href="<?php echo $link; ?>"><?php the_title(); ?></a>
+        </h3>
+<?php
+    endwhile;
+    wp_reset_postdata(); // Reset the post data
+else :
+    echo '<p>No posts found in this category.</p>';
+endif;
+?>
 
-
-
-    if ($category_posts->have_posts()) :
-        while ($category_posts->have_posts()) : $category_posts->the_post();
-        $post_link = get_permalink();
-    ?>
-    <h3><a href=<?php echo $post_link ?>><?php the_title(); ?></a></h3>
-    <?php
-        endwhile;
-        wp_reset_postdata(); // Reset the post data
-    else :
-        echo '<p>No posts found in this category.</p>';
-    endif;
-    ?>
         <div class="link-span"><a  href="<?php echo esc_url(site_url('/vacancy/')); ?>"><?php echo __('More', 'srft-theme' ); ?></a></div>
       </div>
       
