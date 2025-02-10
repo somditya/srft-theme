44714c5c584da38bbd84036d0687fba8545f78a5
somaditya <somditya@gmail.com>
2025-02-10 23:01:40 +0530

    Updates

--- page-general.php
+++ page-general.php
@@ -10,31 +10,28 @@ $page_content = apply_filters('the_content', get_post_field('post_content', $pos
 // Retrieve the featured image URL
 //$featured_image_url = get_the_post_thumbnail_url($post_id, 'large');
 ?>
-    <main>
+<main>
         <!-- Add an image section with the featured image -->
        
 
         <!-- Content area -->
-        <div class="static-container">
+<div class="static-container">
         <div>      
-        <?php
+            <?php
             if ( function_exists('yoast_breadcrumb') ) {
-          yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
-        }
-        ?>
+                yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
+            }
+            ?>
         </div>
         <section class="page-title">
-                <div>
-                    <p class="page-header-text"><?php echo esc_html($post->post_title); ?></p>
-                </div>
-            </section>
+            <div>
+                <p class="page-header-text"><?php echo esc_html(get_the_title($post_id)); ?></p>
+            </div>
         </section>
 
         <section>
-         <?php echo $page_content; ?> 
+            <?php echo $page_content; ?> 
         </section>
-        </div>
-    </main>
 </div>
 
 <?php get_footer();  ?>
