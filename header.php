96bbf74c60850bfc2addee74e50e6723268cdbff
somaditya <somditya@gmail.com>
2025-04-27 23:35:15 +0530

    updates

--- header.php
+++ header.php
@@ -65,7 +65,7 @@ $current_language = get_locale();
       <!--<div class="swiper-slide" >
           <div class="slider-bg">
              <picture>
-             <img  src="<?php bloginfo('template_url'); ?>/images/5.0.jpg" alt="SRFTI delared deemed to be university" >
+             <img  src="<?php bloginfo('template_url'); ?>/images/Cinefondation.jpg" alt="SRFTI delared deemed to be university" >
            </picture>
          </div>
       </div>-->
