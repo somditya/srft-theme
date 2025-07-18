9459a8f07b6b2dbcdace0dd61bb2e3648258ad13
Somaditya Majumder <somditya@gmail.com>
2025-07-18 11:12:24 +0530

    Updates

--- page-admission.php
+++ page-admission.php
@@ -152,6 +152,65 @@ if ($admission_post->have_posts()) {
 wp_reset_postdata();
 ?>
       </div>
+
+  <div class="widget" style="line-height: 1.5">
+                <h4><?php echo __('Sample Question Papers', 'srft-theme'); ?></h4>
+                <?php 
+                // Set document category slug based on language
+                $catslug = ($current_language === 'en_US') ? 'document-en' : 'document-hi';
+
+                $download_post = new WP_Query(array(
+                    'post_type' => 'document',
+                    'tax_query' => array(
+                        array(
+                            'taxonomy' => 'category',
+                            'field'    => 'slug',
+                            'terms'    => $catslug,
+                        ),
+                    ),
+                    'posts_per_page' => -1,       
+                ));
+
+                if ($download_post->have_posts()) {
+                    echo '<ul style="list-style-type: none">';
+                    while ($download_post->have_posts()) {
+                        $download_post->the_post(); 
+                        
+                        // ACF Fields
+                        $document_file = get_field('document');
+                        $document_category = get_field('document-category'); 
+                        $document_description = get_field('document_description');
+
+                        if ($document_category === 'Question Paper') {
+                            if ($document_file) :
+                                // Get the file details
+                                $file_url = $document_file['url'];
+                                $file_id = $document_file['ID'];
+                                $file_size = @filesize(get_attached_file($file_id)); // Suppress errors with @
+                                $file_type_info = wp_check_filetype($file_url);
+                                $file_type = isset($file_type_info['ext']) ? strtoupper($file_type_info['ext']) : 'Unknown';
+                                $file_size_mb = ($file_size !== false) ? size_format($file_size, 2) : 'Unknown';
+                                ?>
+
+                                <li>
+                                    <a href="<?php echo esc_url($file_url); ?>">
+                                        <?php echo esc_html(get_the_title()); ?> 
+                                        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/pdf_icon_resized.png" alt="Download" style="vertical-align: middle;" />
+                                        <?php echo __('Download', 'srft-theme');?> &nbsp; (<?php echo esc_html($file_size_mb); ?>)
+                                    </a>
+                                </li>
+
+                            <?php endif; 
+                        } 
+                    }
+                    echo '</ul>';
+                } else {
+                    echo __('No posts found in the specified category.', 'srft-theme');
+                }
+
+                wp_reset_postdata(); // Reset query
+                ?>   
+            </div>    
   </div>
 
     <div class="main-content" >
