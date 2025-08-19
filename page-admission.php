719a0c8695f6ff120be2cd1d34010bd9c7ce11b2
Somaditya Majumder <somditya@gmail.com>
2025-08-19 17:05:38 +0530

    Updates

--- page-admission.php
+++ page-admission.php
@@ -96,7 +96,7 @@ $current_language = get_locale();
                     $file_size_mb = $file_size ? size_format($file_size, 2) : 'Unknown';
                     ?>
                     <li>
-                        <a href="<?php echo esc_url($file_url); ?>" target="_blank" rel="noopener" title="PDF, opens in a new tab">
+                        <a href="<?php echo esc_url($file_url); ?>" target="_blank" rel="noopener" title="opens in a new tab">
                             <?php echo esc_html(get_the_title()); ?>
                             (<?php echo esc_html($file_type); ?> - <?php echo esc_html($file_size_mb); ?>)
                             <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/pdf_icon_resized.png" alt="" style="vertical-align: middle;" />
