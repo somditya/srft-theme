663234dbdbb4922bd53cfb685536829b5c861f54
somaditya <somditya@gmail.com>
2025-08-24 21:33:28 +0530

    Updates

--- page-directory.php
+++ page-directory.php
@@ -170,7 +170,7 @@ $current_language = get_locale();
                         if ($has_posts) {
                             echo '<h3>' . esc_html($department) . '</h3>';
                             echo '<div class="wrapper">';
-                            echo '<div class="Rtable Rtable--6cols Rtable--collapse">';
+                            echo '<div class="Rtable Rtable--6cols .table-container">';
                             echo '<table style="width: 100%;">';
                             echo '<caption class="sr-only">table showing directory information of the'. esc_html($department) .  '</caption>';
                             echo '<thead>';
