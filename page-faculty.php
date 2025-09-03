e3c4e2b42740de95e8f0db0e8388bc966d6bdcc0
somaditya <somditya@gmail.com>
2025-09-04 00:14:42 +0530

    updates

--- page-faculty.php
+++ page-faculty.php
@@ -31,11 +31,11 @@ $category_id = get_category_ID($category_name);
       <section class="cine-header" style="background-image: url('<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'large')); ?>');">
         <div class="page-banner">
           <h1 class="page-banner-title"><?php echo __('Faculty', 'srft-theme' ); ?></h1>
+        </div>  
       </section>
 
       <section class="section-home">
-        <div class="container" style="padding: 0 3.2rem
-32px;">
+        <div class="container" style="padding: 0 3.2rem;">
         <div class="container-aligned">
     <div class="breadcrumbs-wrapper">
     <?php
