fb259eb6df5cff6fca29075a7fe4a9c91a1112e5
Somaditya Majumder <somditya@gmail.com>
2025-09-09 17:15:41 +0530

    Updates

--- header-demo.php
+++ header-demo.php
@@ -1,15 +1,7 @@
 <?php
-/**
- * The header.
- *
- * This is the template that displays all of the <head> section and everything up until main.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- **/
+/*
+Template Name: Header-Demo
+*/
 ?>
 <?php 
 $current_language = get_locale();
