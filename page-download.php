b3482720a589232197aaaf1092ec7aeb19e848b1
Somaditya Majumder <somditya@gmail.com>
2025-09-17 15:32:37 +0530

    Updates

--- page-download.php
+++ page-download.php
@@ -51,17 +51,20 @@ $category_id = get_category_ID($category_name);
                 <input type="text" id="filterField" ng-model="filterField" placeholder="Search by keyword" ng-change="applyFilters()">
               </label>
             </p>
-
+            <p id="searchInstruction" class="sr-only">
+            <?php echo esc_html__( 'Results update automatically as you type.', 'srft-theme' ); ?>
+           </p>
+          <div class="sr-only" aria-live="polite" role="status" id="searchStatus"> {{ statusMessage }}</div>
             <div class="wrapper" style="padding: 0 3.2rem;">
               <div class="table-container">
                 <table>
                 <caption class="sr-only"><?php echo esc_html__( 'table showing list of tender documents', 'srft-theme' ); ?></caption>
                 <thead>
                 <tr class="Rtable-row Rtable-row--head">
-                  <th class="Rtable-cell location-cell column-heading"><?php echo __('SL.No.', 'srft-theme'); ?></th>
-                  <th class="Rtable-cell name-cell column-heading"><?php esc_html__('Title', 'srft-theme'); ?></th>
-                  <th class="Rtable-cell tenure-cell column-heading"><?php echo __('Category', 'srft-theme'); ?></th>
-                  <th class="Rtable-cell access-link-cell column-heading"><?php echo __('Document', 'srft-theme'); ?></th>
+                  <th class="Rtable-cell location-cell column-heading" scope="col"><?php echo __('SL.No.', 'srft-theme'); ?></th>
+                  <th class="Rtable-cell name-cell column-heading" scope="col"><?php esc_html__('Title', 'srft-theme'); ?></th>
+                  <th class="Rtable-cell tenure-cell column-heading" scope="col"><?php echo __('Category', 'srft-theme'); ?></th>
+                  <th class="Rtable-cell access-link-cell column-heading" scope="col"><?php echo __('Document', 'srft-theme'); ?></th>
                 </tr>
                </thead>
                <tbody>
@@ -69,9 +72,9 @@ $category_id = get_category_ID($category_name);
                   <td class="Rtable-cell location-cell">
                     <div class="Rtable-cell--content"><span class="SL">{{$index + 1}}</span></div>
                   </td>
-                  <td class="Rtable-cell name-cell">
+                  <th class="Rtable-cell name-cell" scope="row">
                     <div class="Rtable-cell--content">{{ document.title }}</div>
-                  </td>
+                  </th>
                   <td class="Rtable-cell tenure-cell">
                     <div class="Rtable-cell--content"><span>{{ getLocalizedCategory(document.category) }}</span></div>
                   </td>
@@ -94,21 +97,25 @@ $category_id = get_category_ID($category_name);
             </div>
 
             <!-- Pagination -->
-            <ul class="pagination">
-              <li ng-class="{ 'disabled': currentPage === 1 }">
-                <a href="#" ng-click="prevPage()" aria-label="<?php echo __('Previous Page', 'srft-theme'); ?>">
-                  <i class="fa fa-chevron-left" style="color: #8b5b2b;"></i>
-                </a>
-              </li>
-              <li ng-repeat="page in getPageNumbers()" ng-class="{ 'active': currentPage === page }">
-                <a href="#" ng-click="setPage(page)">{{ page }}</a>
-              </li>
-              <li ng-class="{ 'disabled': currentPage === totalPages }">
-                <a href="#" ng-click="nextPage()" aria-label="<?php echo __('Next Page', 'srft-theme'); ?>">
-                  <i class="fa fa-chevron-right" style="color: #8b5b2b;"></i>
-                </a>
-              </li>
-            </ul>
+            <nav aria-label="pagination">
+              <ul class="pagination">
+                <li data-ng-class="{ 'disabled': currentPage === 1 }">
+                  <a href="#" aria-label="<?php echo esc_attr__('First Page', 'srft-theme'); ?>" data-ng-click="firstPage()"><i class="fa fa-step-backward" style="color:#8b5b2b;"></i></a>
+                </li>
+                <li data-ng-class="{ 'disabled': currentPage === 1 }">
+                  <a href="#" aria-label="<?php echo esc_attr__('Previous Page', 'srft-theme'); ?>" data-ng-click="prevPage()"><i class="fa fa-chevron-left" style="color:#8b5b2b;"></i></a>
+                </li>
+                <li data-ng-repeat="page in getPageNumbers()" data-ng-class="{ 'active': currentPage === page }">
+                  <a href="#" data-ng-click="setPage(page)" ng-attr-aria-current="{{ currentPage === page ? 'page' : undefined }}">{{ page }}</a>
+                </li>
+                <li data-ng-class="{ 'disabled': currentPage === totalPages }">
+                  <a href="#" aria-label="<?php echo esc_attr__('Next Page', 'srft-theme'); ?>" data-ng-click="nextPage()"><i class="fa fa-chevron-right" style="color:#8b5b2b;"></i></a>
+                </li>
+                <li data-ng-class="{ 'disabled': currentPage === totalPages }">
+                  <a href="#" aria-label="<?php echo esc_attr__('Last Page', 'srft-theme'); ?>" data-ng-click="lastPage()"><i class="fa fa-step-forward" style="color:#8b5b2b;"></i></a>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
@@ -128,6 +135,7 @@ $category_id = get_category_ID($category_name);
         $scope.filterField = '';
         $scope.itemsPerPage = 20;
         $scope.currentPage = 1;
+        $scope.statusEl="";
 
         function bytesToMB(bytes) {
           return (bytes / 1048576).toFixed(2);
@@ -189,6 +197,16 @@ $category_id = get_category_ID($category_name);
 
     return localizedCategories[category] || category; // Return the translated category or default to the original
 };
+
+    function setStatusMessage() {
+        var statusEl = document.getElementById('searchStatus');
+        if (!statusEl) return;
+        if ($scope.filteredTender.length > 0) {
+          statusEl.textContent = $scope.filteredTender.length + " <?php echo esc_js( __( 'tenders found.', 'srft-theme' ) ); ?>";
+        } else {
+          statusEl.textContent = "<?php echo esc_js( __( 'No tenders found.', 'srft-theme' ) ); ?>";
+        }
+      }
 
         // Apply filters when search field changes
         $scope.applyFilters = function () {
