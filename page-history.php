dcdef1c3f50430fd81ce53f24bcc888bbe0908c9
Somaditya Majumder <somditya@gmail.com>
2025-10-03 14:11:03 +0530

    Updates

--- page-history.php
+++ page-history.php
@@ -151,9 +151,9 @@ $page_content = apply_filters('the_content', $post->post_content);
     </div>
 </section>
 
-<div id="picModal" role="dialogue" class="hidden modal-overlay" aria-hidden="true"  aria-modal="true">
+<div id="picModal" role="dialog" class="hidden modal-overlay" aria-hidden="true"  aria-modal="true">
   <div class="modal-content">
-    <button class="close" aria-label="Close modal">✕</button>
+    <button class="close" aria-label="View full image">✕</button>
     <div class="modal-body"></div>
   </div>
 </div>
