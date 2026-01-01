ced49ba30e9ef472dcf9417f24b71acd8edac75a
Somaditya Majumder <somditya@gmail.com>
2026-01-01 17:28:43 +0530

    updates

--- page-aboutgen.php
+++ page-aboutgen.php
@@ -12,10 +12,10 @@ $current_language = get_locale();
 <main>
     <section class="cine-header" style="background-image: url('<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'large')); ?>');">
         <div class="page-banner">
-           <?php
+           <h1 class="page-banner-title"><?php
             $banner_title = get_post_meta( get_the_ID(), 'page-banner-title', true );
-            echo esc_html( $banner_title );
-        ?>
+              echo esc_html( $banner_title );
+           ?> </h1>
         </div>
     </section>
     <div class="container-aligned">
